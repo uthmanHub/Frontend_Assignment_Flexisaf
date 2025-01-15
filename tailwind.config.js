2f66b4c90bf59c1d59acfc77fca91d34e00be875
uthmanHub <uthmancod@gmail.com>
2025-01-15 16:55:06 +0100

    implement an array in a simple webpage using react.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        bg: '#26343e',
+        primary: '#3ebbd6',
+        card: '#fdcb5b',
+        link: '#8a638e'
+      }
+    },
+  },
+  plugins: [],
+}
+
